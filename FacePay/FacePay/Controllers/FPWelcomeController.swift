dd64bd2457080c7e839187e30b76066dfe49667c
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2017-10-20 21:56:24 -0500

    init files with building block build functionalities

---
+++ FacePay/FacePay/Controllers/FPWelcomeController.swift
@@ -0,0 +1,18 @@
+//
+//  FPWelcomeView.swift
+//  FacePay
+//
+//  Created by Bruce Brookshire on 10/20/17.
+//  Copyright © 2017 VandyHacks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FPWelcomeController:    UIViewController
+
+{
+    
+    
+    
+}
